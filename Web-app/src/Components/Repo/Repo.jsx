25bf97214b5ad8d960cc9355796aa984573400aa
KoredeKizzy <akanbi.korede@gmail.com>
2024-05-09 17:46:46 +0100

    ALT-Portfolio

--- Web-app/src/Components/Repo/Repo.jsx
+++ Web-app/src/Components/Repo/Repo.jsx
@@ -4,8 +4,13 @@ import '../Repo/Repo.css';
 
 function App() {
   const [repositories, setRepositories] = useState([]);
-  const [loading, setLoading] = useState([true])
   const token = process.env.REACT_APP_GITHUB_TOKEN;
+  const [currentPage, setCurrentPage] = useState(1);
+  const [itemsPerPage] = useState(3); // Display 3 items per page
+
+  const indexOfLastItem = currentPage * itemsPerPage;
+  const indexOfFirstItem = indexOfLastItem - itemsPerPage;
+  const currentItems = repositories.slice(indexOfFirstItem, indexOfLastItem);
 
   useEffect(() => {
     async function fetchRepositories() {
@@ -17,40 +22,31 @@ function App() {
         });
         const data = await response.json();
         setRepositories(data);
-        setLoading(false);
       } catch (error) {
         console.error('Error fetching repositories:', error);
-        setLoading(false);
       }
     }
   
     fetchRepositories(); // Call fetchRepositories inside useEffect
   
   }, [token]); // Include token in the dependency array
+
+  // Calculate total pages based on the number of items and items per page
+  const totalPages = Math.ceil(repositories.length / itemsPerPage);
+
+  const nextPage = () => {
+    setCurrentPage(prevPage => Math.min(prevPage + 1, totalPages));
+  };
+
+  const prevPage = () => {
+    setCurrentPage(prevPage => Math.max(prevPage - 1, 1)); // Ensure current page doesn't go below 1
+  };
   
-
-  async function fetchRepositories(KoredeKizzy) {
-    try {
-      const response = await fetch('https://api.github.com/users/KoredeKizzy/ALT-Portfolio', {
-        headers: {
-          Authorization: `Bearer ${token}` // Use the token in the request headers
-        }
-      });
-      const data = await response.json();
-      setRepositories(data);
-
-    } catch (error) {
-      console.error('Error fetching repositories:', error);
-      
-    }
-  }
-  
-
   return (
     <div className="container">
       <h1>GitHub Repositories</h1>
       <div id="repositories">
-        {repositories.map(repository => (
+        {currentItems.map(repository => (
           <div key={repository.id} className="repository">
             <h3>{repository.name}</h3>
             <p>{repository.description || "No description"}</p>
@@ -58,6 +54,10 @@ function App() {
           </div>
         ))}
       </div>
+      <><br /></>
+      <button onClick={prevPage} disabled={currentPage === 1}>Previous</button>
+      <span>Page {currentPage} of {totalPages}</span>
+      <button onClick={nextPage} disabled={currentPage === totalPages}>Next</button>
     </div>
   );
 }

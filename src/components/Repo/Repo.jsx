58f73605d06cdb3827200128e0d393754e9045c9
KoredeKizzy <akanbi.korede@gmail.com>
2024-05-20 19:18:08 +0100

    Latest commit

--- src/components/Repo/Repo.jsx
+++ src/components/Repo/Repo.jsx
@@ -3,7 +3,6 @@ import '../Repo/Repo.css';
 
 function App() {
   const [repositories, setRepositories] = useState([]);
-  const token = process.env.REACT_APP_API_KEY;
   const [currentPage, setCurrentPage] = useState(1);
   const [itemsPerPage] = useState(3); // Display 3 items per page
 
@@ -17,20 +16,16 @@ function App() {
   useEffect(() => {
     async function fetchRepositories() {
       try {
-        const response = await fetch('https://api.github.com/users/KoredeKizzy/repos', {
-          headers: {
-            Authorization: `Bearer ${token}`
-          }
-        });
+        const response = await fetch('https://api.github.com/users/KoredeKizzy/repos');
         const data = await response.json();
         setRepositories(data);
       } catch (error) {
         console.error('Error fetching repositories:', error);
       }
     }
-  
+
     fetchRepositories(); // Call fetchRepositories inside useEffect
-  }, [token]); // Include token in the dependency array
+  }, []); // Removed token from the dependency array
 
   const nextPage = () => {
     setCurrentPage(prevPage => Math.min(prevPage + 1, totalPages));
@@ -39,24 +34,23 @@ function App() {
   const prevPage = () => {
     setCurrentPage(prevPage => Math.max(prevPage - 1, 1)); // Ensure current page doesn't go below 1
   };
-  
+
   return (
     <div className="container">
-      {/* <h1>GitHub Repositories</h1> */}
       <div id="repositories">
         {currentItems.map(repository => (
-          <div key={repository.id} className="repository" >
+          <div key={repository.id} className="repository">
             <h3>{repository.name}</h3>
             <p>{repository.description || "No description"}</p>
             <p>Stars: {repository.stargazers_count} | Forks: {repository.forks_count}</p>
           </div>
         ))}
       </div>
-      <><br /></>
+      <br />
       <div className="buttonn">
-      <button className="button" onClick={prevPage} disabled={currentPage === 1}>Previous</button>
-      <span>Page {currentPage} of {totalPages}</span>
-      <button className="button" onClick={nextPage} disabled={currentPage === totalPages}>Next</button>
+        <button className="button" onClick={prevPage} disabled={currentPage === 1}>Previous</button>
+        <span>Page {currentPage} of {totalPages}</span>
+        <button className="button" onClick={nextPage} disabled={currentPage === totalPages}>Next</button>
       </div>
     </div>
   );
